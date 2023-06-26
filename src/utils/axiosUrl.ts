3b301a3d55e3e42ed6663b8157bd5c42fbbfdf87
ScriptedPranav <sinhafighterpranav@gmail.com>
2023-06-26 18:54:38 +0530

    local axios url

--- src/utils/axiosUrl.ts
+++ src/utils/axiosUrl.ts
@@ -1,4 +1,4 @@
-const url = "https://audiogpt-uhfc.onrender.com/api/bot";
-// const url = "http://localhost:3000/api/bot"
+// const url = "https://audiogpt-uhfc.onrender.com/api/bot";
+const url = "http://localhost:3000/api/bot"
 
 export default url;
